be72535d1e5d26dd35e2962330b3fa854b3bb95a
Kroogle7 <ip_vturin@mail.ru>
2022-06-05 19:44:51 +0300

    add digital signature

--- server.py
+++ server.py
@@ -1,11 +1,31 @@
-from ast import Delete
-from re import template
 from typing import Optional
-from unicodedata import name
+
+import base64
+import hmac
+import hashlib
+
 from fastapi import FastAPI, Form, Cookie
 from fastapi.responses import Response
 
 app = FastAPI()
+
+
+SECRET_KEY = "8f1f127477ce3681e45dfb5cbd197cb7aa917dd03d4647c39541f861ce223677"
+
+def sign_data(data:str) ->str:
+    '''Возвращает подписанные данные data'''
+    return hmac.new(
+        SECRET_KEY.encode(),
+        msg =data.encode(),
+        digestmod = hashlib.sha256
+        ).hexdigest().upper()
+
+def get_username_from_signed_string(username_signed:str) -> Optional[str]:
+    username_base64 , sign = username_signed.split('.')
+    username = base64.b64decode(username_base64.encode()).decode()
+    valid_sign = sign_data(username)
+    if hmac.compare_digest(valid_sign, sign):
+        return username
 
 users = {
     "kirill@user.com":{
@@ -27,14 +47,22 @@ def index_page(username: Optional[str] = Cookie(default=None)):
     with open('templates/index.html', 'r') as f:
         login_page = f.read()
         if username:
-            try:
-                user = users[username]
-            except KeyError:
-                response =  Response(login_page,media_type='text/html')
-                response.delete_cookie(key='username')
-                return response
-            return Response(f"Hello, {users[username]['name']}!",media_type='text/html')
-    return Response(login_page, media_type='text/html')
+             return Response(login_page, media_type='text/html')
+        valid_username = get_username_from_signed_string(username)
+        if not valid_username:
+            response =  Response(f"Hello, {users[username]['name']}!",media_type='text/html')
+            response.delete_cookie(key='username')
+            return response
+
+
+        try:
+            user = users[valid_username]
+        except KeyError:
+            response =  Response(login_page,media_type='text/html')
+            response.delete_cookie(key='username')
+            return response
+        return Response(f"Hello, {users[valid_username]['name']}!",media_type='text/html')
+
 
 @app.post('/login')
 def process_login_page(username: str = Form(...), password: str = Form(...)):
@@ -45,6 +73,9 @@ def process_login_page(username: str = Form(...), password: str = Form(...)):
     response =  Response(
         f"Hello, {user['name']}<br /> Balance:{user['balance']}",
         media_type='text/html')
-    response.set_cookie(key='username',value=username)
+
+    username_signed = base64.b64encode(username.encode()).decode()+ '.' + \
+        sign_data(username)    
+    response.set_cookie(key='username',value=username_signed)
     return response
 
